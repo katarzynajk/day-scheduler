5eb3cf7fcdc2e1f180c70d383857a37483bb6f23
Katarzyna Jaromin-Kaliciak <jarominyanni@gmail.com>
2022-06-11 18:25:40 +0100

    prepare the time table in index.html file, add working hous label, document on ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
+/* current date & time */
 const renderDate = () => {
   const date = moment().format("Do of MMMM YYYY HH:mm");
   $("#currentDay").append(date);
 };
+
+const timeBlocks = $("#time-blocks");
+
+/* working hour labels */
+const workHours = [
+  { timeLabel: "9am", key: 9 },
+  { timeLabel: "10am", key: 10 },
+  { timeLabel: "11am", key: 11 },
+  { timeLabel: "12pm", key: 12 },
+  { timeLabel: "1pm", key: 13 },
+  { timeLabel: "2pm", key: 14 },
+  { timeLabel: "3pm", key: 15 },
+  { timeLabel: "4pm", key: 16 },
+  { timeLabel: "5pm", key: 17 },
+];
+
+// document on ready
+const onReady = () => {};
 
 $(window).on("load", renderDate);
